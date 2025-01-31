b49302d4ac4d0e8815a575e76eef910cb287a703
anuradhashanthraj <radhar3005@gmail.com>
2025-01-31 11:41:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,6 +32,6 @@ hbs.registerPartials('views/partials')
 mongoose.connect("mongodb://127.0.0.1/restorent",()=>{
     console.log("Server connected..");
 })
-app.listen(5656,()=>{
-    console.log('server is start..')
-})
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Server is running on port 3000');
+});
